d71a473146e3f7886d9873b44bb3b63b537dc7cf
Jzurcc <johnreniel042206@gmail.com>
2024-03-24 03:28:11 +0800

    Added map memorizations

--- obj/Debug/net8.0/Kingdom of Nohwyrr.AssemblyInfo.cs
+++ obj/Debug/net8.0/Kingdom of Nohwyrr.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Kingdom of Nohwyrr")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fe9887a4562a4090139c11404dfe2d5e335cb3a5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3d1f0ed9c7cfcf8453e1010c2e7e4cb579ec1ede")]
 [assembly: System.Reflection.AssemblyProductAttribute("Kingdom of Nohwyrr")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Kingdom of Nohwyrr")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
